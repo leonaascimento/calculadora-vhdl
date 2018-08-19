f1e1c0344407a7c22e4e2babed56004c16a8c21c
Leonardo Nascimento <leonaascimento@gmail.com>
2018-08-19 17:09:06 -0300

    Added manual sum

--- src/adder/adder.vhd
+++ src/adder/adder.vhd
@@ -6,17 +6,28 @@ entity adder is
 	generic (
 		sizeof_operand : natural := 8);
 	port (
-		first, second : in signed(sizeof_operand - 1 downto 0);
+		first, second : in  signed(sizeof_operand - 1 downto 0);
 		result        : out signed(sizeof_operand - 1 downto 0);
 		overflow      : out std_logic);
 end adder;
 
 architecture behavior of adder is
-	signal aux_result : signed(sizeof_operand - 1 downto 0);
+	signal sum   : signed(sizeof_operand - 1 downto 0);
+	signal carry : std_logic_vector(sizeof_operand + 1 downto 0) := (others => '0');
 begin
-	aux_result <= first + second;
-	result <= aux_result;
+	result <= sum;
+	overflow <= (first(first'high) xnor second(second'high))
+		and (first(first'high) xor sum(sum'high));
 	
-	overflow <= (first(first'high) xnor second(second'high)) and
-					(first(first'high) xor aux_result(aux_result'high));
+	process(first, second, carry)
+	begin
+		for i in sum'reverse_range loop
+			sum(i) <= first(i)
+				xor (second(i) xor carry(i));
+				
+			carry(i + 1) <= (second(i) and carry(i))
+				or (second(i) and first(i))
+				or (carry(i) and first(i));
+		end loop;
+	end process;
 end behavior;
