85e766d86da42a0b49b33334ca95ea5fab9c111e
Leonardo Nascimento <leonaascimento@gmail.com>
2018-08-20 11:08:56 -0300

    Enable buffer ports for testing

--- src/calculadora/simulation/qsim/work/calculadora/_primary.vhd
+++ src/calculadora/simulation/qsim/work/calculadora/_primary.vhd
@@ -10,6 +10,22 @@ entity calculadora is
         hex3            : out    vl_logic_vector(6 downto 0);
         hex2            : out    vl_logic_vector(6 downto 0);
         hex1            : out    vl_logic_vector(6 downto 0);
-        hex0            : out    vl_logic_vector(6 downto 0)
+        hex0            : out    vl_logic_vector(6 downto 0);
+        stack_push_value: out    vl_logic_vector(7 downto 0);
+        stack_push      : out    vl_logic;
+        stack_pop       : out    vl_logic;
+        set_operator    : out    vl_logic;
+        operator        : out    vl_logic_vector(1 downto 0);
+        first_operand   : out    vl_logic_vector(7 downto 0);
+        second_operand  : out    vl_logic_vector(7 downto 0);
+        operation_result: out    vl_logic_vector(7 downto 0);
+        operation_overflow: out    vl_logic;
+        bcd_result      : out    vl_logic_vector(11 downto 0);
+        hex3_result_sign: out    vl_logic_vector(6 downto 0);
+        hex2_result     : out    vl_logic_vector(6 downto 0);
+        hex1_result     : out    vl_logic_vector(6 downto 0);
+        hex0_result     : out    vl_logic_vector(6 downto 0);
+        pwm_out         : out    vl_logic;
+        pwm_out_vector  : out    vl_logic_vector(6 downto 0)
     );
 end calculadora;
